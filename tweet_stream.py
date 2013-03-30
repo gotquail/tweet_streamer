10bf0cfe72ac2982d4c2339e7999ab489f5d48ba
Ray Kellough <raykellough@gmail.com>
2013-03-29 23:30:10 -0700

    just a little cleaner... still don't need the index, but it's there if you only want x tweets

--- tweet_stream.py
+++ tweet_stream.py
@@ -7,6 +7,7 @@ def main():
 
 	stream = tweetstream.SampleStream("BigBrother2984", "had416twitter");
 	f = open('tweets.txt', 'w')
+
 	i = 0
 	for tweet in stream:
 		if (i >= NUM_TWEETS):
@@ -16,7 +17,7 @@ def main():
 		if 'lang' in tweet:
 			if tweet['lang'] != 'en':
 				continue
-				
+
 			t = str(tweet).encode('ascii', 'ignore') # encode to ascii
 			f.write(str(t) + '\n')
 
@@ -25,6 +26,9 @@ def main():
 		# if 'text' in tweet:
 		# 	f.write(str(tweet['text'].encode('ascii', 'ignore')) + "\n")
 
+		# i += 1
+
+	f.close()
 	return 0
 
 
